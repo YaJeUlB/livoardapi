a89e2b5a6394b7b0ba49427bd9c683055eecbb10
user <user@userui-iMac.local>
2025-09-18 21:30:51 +0900

    Initial commit

---
+++ src/main/java/com/livoard/livoardapi/LivoardapiApplication.java
@@ -0,0 +1,13 @@
+package com.livoard.livoardapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivoardapiApplication {
+
+	public static void main(String[] args) {
+        SpringApplication.run(LivoardapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/livoard/livoardapi/LivoardapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.livoard.livoardapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivoardapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
